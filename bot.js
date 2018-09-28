41aacdaee9d3508b500dead759953f711ee2af8f
Nolan Perry <nperry@extinctiongaming.com>
2018-09-28 01:13:14 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,21 +6,15 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-
-  // This is the best way to define args. Trust me.
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  // The list of if/else is replaced with those simple 2 lines:
-  try {
-    let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }
+client.on("message", (message) => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  if(command === 'ping') {
+  message.channel.send('Pong!');
+  } else
+if (command === 'blah') {
+  message.channel.send('Meh.');
+}
 });
 
 client.login(config.token);
