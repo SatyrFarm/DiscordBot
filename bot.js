21b8f898b02fdfb5329548cbbd51e6deee5f750c
Nolan Perry <nperry@extinctiongaming.com>
2018-09-28 01:40:01 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,32 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const silly = require("./silly.json");
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-  if(command === 'ping') {
-  message.channel.send('Pong!');
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "help")) {
+    message.channel.send(silly.help);
   } else
-if (command === 'blah') {
-  message.channel.send('Meh.');
-}
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(silly.welcome + ', ${member}' );
 });
 
 client.login(config.token);
