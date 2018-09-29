455581e6fb19386c773f58bff5519611e1459f93
Cody Cooper <nperry@extinctiongaming.com>
2018-09-29 18:20:15 +0000

    Change prune
    make prune disabled by default

--- commands/prune.js
+++ commands/prune.js
@@ -11,7 +11,7 @@ module.exports = {
 			return message.reply('you need to input a number between 1 and 99.');
 		}
 
-		message.channel.bulkDelete(amount, true).catch(err => {
+		// message.channel.bulkDelete(amount, true).catch(err => {
 			console.error(err);
 			message.channel.send('there was an error trying to prune messages in this channel!');
 		});
