4118273eede002335b5aa05aec93a53a12e5391f
Reid Luna <reidluna@icloud.com>
2018-09-29 10:48:28 -0600

    Add Comments
    ## I just added comments.

--- commands/top20.js
+++ commands/top20.js
@@ -1,5 +1,5 @@
-const { RichEmbed } = require("discord.js");
-const request = require("request-promise");
+const { RichEmbed } = require("discord.js"); // Getting RichEmbed from discord.js. Same as Discord.RichEmbed()
+const request = require("request-promise"); // A new async request module
 
 const embed = new RichEmbed()
   .setColor("#0099ff")
@@ -23,19 +23,19 @@ const embed = new RichEmbed()
 
 const createEmbed = async () => {
   try {
-    const users = await request("http://opensimworld.com/farmstats/top.json");
-    users.forEach((user, i) => {
-      if (i > 19) return;
-      const { username, display_name, farmPoints } = user;
-      const name = display_name ? display_name : username;
+    const users = await request("http://opensimworld.com/farmstats/top.json"); // users will be the body of the request
+    users.forEach((user, i) => { // loops through all the users
+      if (i > 19) return; // since the index is 0-filed, it will start at 0 and go to 19 for 20 users
+      const { username, display_name, farmPoints } = user; // destructures the user object into username, display_name, and points
+      const name = display_name ? display_name : username; // if there is a display_name show that, if not show username
       embed.addField(
         name,
-        farmPoints.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        farmPoints.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") // adds commas for numbers
       );
     });
   } catch (e) {
     console.log(
-      `Failure loading "http://opensimworld.com/farmstats/top.json". Error: ${e}`
+      `Failure loading "http://opensimworld.com/farmstats/top.json". Error: ${e}` // if the request returns anything but code: 200
     );
   }
 };
@@ -45,7 +45,7 @@ module.exports = {
   description: "Allows you to view the top20 Satyr Farm Players",
   cooldown: 5,
   async execute(message) {
-    await createEmbed();
-    message.channel.send({ embed });
+    await createEmbed(); // waits for the embed to be created
+    message.channel.send({ embed }); // sends the embed. exactly the same as message.channel.send({embed: embed});
   }
 };
