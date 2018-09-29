b429cabc6bc829ca0f2f85055085d0c3e4cbbfc6
Cody Cooper <nperry@extinctiongaming.com>
2018-09-29 15:58:56 +0000

    update

--- commands/top20.js
+++ commands/top20.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js');
+const request = require("request");
 
+request("http://opensimworld.com/farmstats/top.json", function(error, response, body) {
+  // console.log(body);
+  jsontop = body;
+});
 const leaderboardEmbed = new Discord.RichEmbed()
     .setColor('#0099ff')
     .setTitle('Satyr Far Top20')
@@ -17,13 +22,12 @@ const leaderboardEmbed = new Discord.RichEmbed()
     .setFooter('Statistics are up to date as of the above timestamp', 'https://i.imgur.com/wSTFkRM.png');
 
 
-
 module.exports = {
 	name: 'top20',
 	description: 'Allows you to view the top20 Satyr Farm Players',
 	cooldown: 5,
 	execute(message) {
-		message.channel.send('The Top20 Players in Satyr Farm are');
+		message.channel.send(jsontop);
 		message.channel.send(leaderboardEmbed);
 
 	},
