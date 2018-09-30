c5df28fed3c4a39496b9a08fd94586ad4fc61e59
Cody Cooper <nperry@extinctiongaming.com>
2018-09-30 20:09:57 +0000

    Update 0.0.3 Sync

--- bot.js
+++ bot.js
@@ -77,4 +77,13 @@ client.on('message', message => {
 	}
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send( config.welcome + ` , ${member}`);
+});
+
 client.login(config.token);
