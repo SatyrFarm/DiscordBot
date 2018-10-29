2a3ed3757028474f7ef7b9f8c3f1df63ccdb92ec
Nolan Perry <nperry@extinctiongaming.com>
2018-10-29 08:02:31 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,35 @@
 module.exports = {
-    "extends": ["eslint:recommended", "google"],
-    "rules": {
-      // Additional, per-project rules...
- 
-    },
-    "parserOptions":{
-      "ecmaVersion": 6
-    }
+  'extends': [
+    'eslint:recommended', 'google'
+  ],
+  'rules': {
+    // Additional, per-project rules...
+    'no-undef': 'error',
+    'no-console': 'off',
+    "max-len": [
+      2, {
+        "code": 250,
+        "tabWidth": 4,
+        "ignoreUrls": true
+      }
+    ],
+    "require-jsdoc": [
+      "error", {
+        "require": {
+          "FunctionDeclaration": false,
+          "MethodDefinition": false,
+          "ClassDeclaration": false,
+          "ArrowFunctionExpression": false,
+          "FunctionExpression": false
+        }
+      }
+    ]
+  },
+  'env': {
+    'node': true,
+    'es6': true
+  },
+  'parserOptions': {
+    'ecmaVersion': 2017
   }
+}
