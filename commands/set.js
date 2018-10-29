4eae0a309c8ea459f81f7a951372d99f366b4a4c
Nolan Perry <nperry@extinctiongaming.com>
2018-10-29 08:56:24 -0400

    Update set.js

--- commands/set.js
+++ commands/set.js
@@ -1,9 +1,9 @@
-const {inspect} = require('util');
+const {inspect} = require("util");
 
 // This command is to modify/edit guild configuration. Perm Level 3 for admins
 // and owners only. Used for changing prefixes and role names and such. Note
-// that there's no 'checks' in this basic version - no config 'types' like Role,
-// String, Int, etc... It's basic, to be extended with your deft hands! Note
+// that there"s no "checks" in this basic version - no config "types" like Role,
+// String, Int, etc... It"s basic, to be extended with your deft hands! Note
 // the **destructuring** here. instead of `args` we have : [action, key,
 // ...value] This gives us the equivalent of either: const action = args[0];
 // const key = args[1]; const value = args.slice(2); OR the same as: const
@@ -18,25 +18,25 @@ exports.run = async (client, message, [
     .get(message.guild.id);
 
   // Edit an existing key value
-  if (action === 'edit') {
+  if (action === "edit") {
     // User must specify a key.
     if (!key) {
-      return message.reply('Please specify a key to edit');
+      return message.reply("Please specify a key to edit");
     }
 
     // User must specify a key that actually exists!
     if (!settings[key]) {
-      return message.reply('This key does not exist in the settings');
+      return message.reply("This key does not exist in the settings");
     }
 
     // User must specify a value to change.
     if (value.length < 1) {
-      return message.reply('Please specify a new value');
+      return message.reply("Please specify a new value");
     }
 
     // User must specify a different value than the current one.
-    if (value.join(' ') === settings[key]) {
-      return message.reply('This setting already has that value!');
+    if (value.join(" ") === settings[key]) {
+      return message.reply("This setting already has that value!");
     }
 
     // If the guild does not have any overrides, initialize it.
@@ -50,57 +50,57 @@ exports.run = async (client, message, [
     // enmap key/value pair.
     client
       .settings
-      .setProp(message.guild.id, key, value.join(' '));
+      .setProp(message.guild.id, key, value.join(" "));
 
     // Confirm everything is fine!
-    message.reply(`${key} successfully edited to ${value.join(' ')}`);
+    message.reply("${key} successfully edited to ${value.join(" ")}");
   } else
 
   // Resets a key to the default value
-  if (action === 'reset') {
+  if (action === "reset") {
     if (!key) {
-      return message.reply('Please specify a key to reset.');
+      return message.reply("Please specify a key to reset.");
     }
     if (!settings[key]) {
-      return message.reply('This key does not exist in the settings');
+      return message.reply("This key does not exist in the settings");
     }
     if (!overrides[key]) {
-      return message.reply('This key does not have an override and is already using defaults.');
+      return message.reply("This key does not have an override and is already using defaults.");
     }
 
     // Good demonstration of the custom awaitReply method in
     // `./modules/functions.js` !
-    const response = await client.awaitReply(message, `Are you sure you want to reset ${key} to the default value?`);
+    const response = await client.awaitReply(message, "Are you sure you want to reset ${key} to the default value?");
 
     // If they respond with y or yes, continue.
-    if (['y', 'yes'].includes(response.toLowerCase())) {
+    if (["y", "yes"].includes(response.toLowerCase())) {
       // We delete the `key` here.
       delete overrides[key];
       client
         .settings
         .set(message.guild.id, overrides);
-      message.reply(`${key} was successfully reset.`);
+      message.reply("${key} was successfully reset.");
     } else
     // If they respond with n or no, we inform them that the action has been
     // cancelled.
-    if (['n', 'no', 'cancel'].includes(response)) {
-      message.reply('Action cancelled.');
+    if (["n", "no", "cancel"].includes(response)) {
+      message.reply("Action cancelled.");
     }
-  } else if (action === 'get') {
+  } else if (action === "get") {
     if (!key) {
-      return message.reply('Please specify a key to view');
+      return message.reply("Please specify a key to view");
     }
     if (!settings[key]) {
-      return message.reply('This key does not exist in the settings');
+      return message.reply("This key does not exist in the settings");
     }
     const isDefault = !overrides[key]
-      ? '\nThis is the default global default value.'
-      : '';
+      ? "\nThis is the default global default value."
+      : "";
     message.reply(`The value of ${key} is currently ${settings[key]}${isDefault}`);
   } else {
     message
       .channel
-      .send(inspect(settings), {code: 'json'});
+      .send(inspect(settings), {code: "json"});
   }
 };
 
@@ -108,14 +108,14 @@ exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [
-    'setting', 'settings', 'conf',
+    "setting", "settings", "conf",
   ],
-  permLevel: 'Administrator',
+  permLevel: "Administrator",
 };
 
 exports.help = {
-  name: 'set',
-  category: 'System',
-  description: 'View or change settings for your server.',
-  usage: 'set <view/get/edit> <key> <value>',
+  name: "set",
+  category: "System",
+  description: "View or change settings for your server.",
+  usage: "set <view/get/edit> <key> <value>",
 };
