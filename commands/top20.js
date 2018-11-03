d73a8fe398d0eb4a74efecca42712ce34fac4fa1
Nolan Perry <nperry@extinctiongaming.com>
2018-11-03 19:02:47 -0400

    Update top20.js

--- commands/top20.js
+++ commands/top20.js
@@ -17,7 +17,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const {RichEmbed } = require('discord.js'); // Getting RichEmbed from discord.js. Same as Discord.RichEmbed()
+const {RichEmbed} = require('discord.js'); // Getting RichEmbed from discord.js. Same as Discord.RichEmbed()
 const request = require('request-promise'); // A new async request module
 
 const embed = new RichEmbed()
