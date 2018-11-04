139788620f4ce114ee25b470718dc34eedabf2e0
Nolan Perry <nperry@extinctiongaming.com>
2018-11-04 10:24:49 -0500

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -4,19 +4,19 @@ const Discord = require('discord.js');
 // BUG: On link post it updates the post to add the embed and triggers this event
 // BUG: Repeats the same message on the Unofficla Node.js Discord Server
 module.exports = async (client, oldMessage, newMessage) => {
-    var old = oldMessage;
-    var new_ = newMessage;
+    let old = oldMessage;
+    let new_ = newMessage;
 
 
-    const logs = oldMessage.guild.channels.find('name', 'logging');
+    let logs = oldMessage.guild.channels.find('name', 'logging');
 
-    const embed = new Discord.RichEmbed().setTitle("Message Update").setAuthor(client.user.username, client.user.avatarURL).setColor(0xff268f);
+    let embed = new Discord.RichEmbed().setTitle("Message Update").setAuthor(client.user.username, client.user.avatarURL).setColor(0xff268f);
     embed.addField("User", old.author.username + "#" + old.author.discriminator);
     embed.addField("Old", old.content);
     embed.addField("New", new_.content);
 
-    var currentdate = new Date();
-    var datetime = currentdate.getDate() + "/" +
+    let currentdate = new Date();
+    let datetime = currentdate.getDate() + "/" +
         (currentdate.getMonth() + 1) + "/" +
         currentdate.getFullYear() + " @ " +
         currentdate.getHours() + ":" +
