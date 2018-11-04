e2428bf3c55118893e8add3d5dc705d9c23f35cf
Nolan Perry <nperry@extinctiongaming.com>
2018-11-04 11:38:21 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,14 +3,11 @@
 module.exports = (client, member) => {
   // Load the guild's settings
   const settings = client.getGuildSettings(member.guild);
-
   // PM The user the rules
   pmUserRules(member, client);
-
   // Give the person the intern role on join
   // NOTE: Only for the Unofficial Node.js Discord Server
   member.addRole(member.guild.roles.find((role) => role.name === 'Farmhand'));
-
   // If welcome is off, don't proceed (don't welcome the user)
   if (settings.welcomeEnabled !== 'true') {
     return;
@@ -19,7 +16,6 @@ module.exports = (client, member) => {
     const welcomeMessage = settings
       .welcomeMessage
       .replace('{{user}}', member);
-
     // Send the welcome message to the welcome channel There's a place for more
     // configs here.
     member
@@ -31,9 +27,12 @@ module.exports = (client, member) => {
     // TODO: Ensure the channel is created
   }
 };
-
+/**
+ * Sends a welcome message to the user who joined 
+ * @param {object} user
+ * @param {object} client
+ */
 function pmUserRules(user, client) {
-
   user.send(`Welcome to our Discord Server, The SatyrFarm/OpenSimWorld Discord Server!
 -----
 Before you start jumping into different rooms and such, go to the bottom left of the webpage/desktop app and click that gear on the bottom left! In there, change your display picture, and whatever settings you may think should be changed. We highly recommend you enable Push To Talk, it'll just help those in the same voice channels if you have a lot of background noise.
