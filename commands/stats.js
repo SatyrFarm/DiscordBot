d8d201871d68d639ebc594f2eaed7e029a9a44e9
Nolan Perry <nperry@bridgespreparatoryschool.org>
2018-11-23 00:06:16 -0500

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,23 +1,38 @@
-const {version} = require('discord.js');
+const {
+  version,
+} = require('discord.js');
+
 const moment = require('moment');
+
 require('moment-duration-format');
+
 const bVersion = require('project-version');
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const duration = moment
     .duration(client.uptime)
     .format(' D [days], H [hrs], m [mins], s [secs]');
+
+  let channelsLen = client.channels;
+  channelsLen = channelsLen.filter((channels) => channels.type !== 'dm' && channels.type !== 'category')
+    .size
+    .toLocaleString();
+
+  const usersLen = message.guild.members.size.toLocaleString();
+
   message
     .channel
     .send(`= STATISTICS =
 • Mem Usage   :: ${ (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • Uptime      :: ${duration}
-• Users       :: ${client.users.size.toLocaleString()}
+• Users       :: ${usersLen}
 • Servers     :: ${client.guilds.size.toLocaleString()}
-• Channels    :: ${client.channels.size.toLocaleString()}
+• Channels    :: ${channelsLen}
 • Bot Version :: v${bVersion}
 • Discord.js  :: v${version}
-• Node        :: ${process.version}`, {code: 'asciidoc'});
+• Node        :: ${process.version}`, {
+      code: 'asciidoc',
+    });
 };
 
 exports.conf = {
